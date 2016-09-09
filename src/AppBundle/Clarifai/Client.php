c89b92a178ebb3c43cf8c2842d305eefe17bd2e6
Johannes <reiners.joh@gmail.com>
2016-09-09 21:40:13 +0200

    service voor clarifai aangemaakt

--- src/AppBundle/Clarifai/Client.php
+++ src/AppBundle/Clarifai/Client.php
@@ -4,6 +4,10 @@ namespace AppBundle\Clarifai;
 
 class Client
 {
+    private $clientId = "QhK6U2u8R17RJbWVt0Mn6QLWOvsKagclh1A4rH8P";
+    private $clientSecret = "ImZ2tKfJht9uWraS6fbjEBjnORq_Rsu_X9txBRPM";
+    private $accessToken = "Zvby0BiaQnTM6khQnRepMjwLAYGst4";
+
     public function getTagsByImageFile(\SplFileInfo $imageFile)
     {
 

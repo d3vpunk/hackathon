83a5165e69822810a91149697617a9dffc294ba9
Marco Ranter <marco.ranter@webstores.nl>
2016-09-09 21:31:21 +0200

    initial commit

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+
+    public function listAction()
+    {
+        return new Response();
+    }
+
+}

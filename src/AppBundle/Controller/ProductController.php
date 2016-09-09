4ccc2d51e05071b606081753cff017564cb774bf
Marco Ranter <marco.ranter@webstores.nl>
2016-09-09 22:00:05 +0200

    image form

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\ImageType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -12,5 +14,20 @@ class ProductController extends Controller
     {
         return new Response();
     }
+
+    public function uploadAction(Request $request)
+    {
+        $imageForm = $this->createForm(ImageType::class);
+        $imageForm->handleRequest($request);
+
+        if ($imageForm->isSubmitted()) {
+            $imageAiRatings = $this->get('clarifai.client')->getTagsByImageFile($imageForm->get('file')->getData());
+        }
+
+        return $this->render('AppBundle:Product:upload.html.twig', [
+            'imageForm' => $imageForm->createView(),
+        ]);
+    }
+
 
 }

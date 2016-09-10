35000a51115b9241d78198b1ca9879e45b3a4e40
Johannes <reiners.joh@gmail.com>
2016-09-10 10:28:53 +0200

    access key wordt gegenereerd

--- src/AppBundle/Clarifai/Client.php
+++ src/AppBundle/Clarifai/Client.php
@@ -10,6 +10,32 @@ class Client
 
     public function getTagsByImageFile(\SplFileInfo $imageFile)
     {
+        $accessToken = $this->getAccessToken();
+    }
 
+    private function getAccessToken()
+    {
+
+        $accessToken = "";
+
+        $ch = curl_init();
+
+        $postVars = sprintf("client_id=%s&client_secret=%s&grant_type=client_credentials", $this->clientId, $this->clientSecret);
+
+        curl_setopt($ch, CURLOPT_URL,"https://api.clarifai.com/v1/token/");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+            $postVars);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec ($ch);
+
+        var_dump($response);
+        die();
+
+        curl_close ($ch);
+
+        return $accessToken;
     }
 }
